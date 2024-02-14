c2fa86e6e13a075bee5e2ae8e2316e47f64efe07
YUBESHBHATTARAI <yubeshbhattarai@gmail.com>
2024-02-14 15:46:54 +0545

    add

--- add.c
+++ add.c
@@ -1,1 +1,10 @@
-a=int
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter the values:\n");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("the entered sum is %d",sum);
+    return 0;
+}
