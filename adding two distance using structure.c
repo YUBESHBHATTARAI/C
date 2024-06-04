66739d6449c7b6b773ccc09cc72f15030414709b
YUBESHBHATTARAI <yubeshbhattarai@gmail.com>
2024-06-04 22:15:43 +0545

    .

---
+++ adding two distance using structure.c
@@ -0,0 +1,27 @@
+//Write aC program to add two distances(feet-inch system) enter by user,usingstructures.
+#include<stdio.h>
+struct distance
+{
+int feet;
+int inch;
+};
+
+int main()
+{
+// Declaring structure variables
+struct distance d1, d2, sum;
+// Inputs
+printf("Enter feet and inch of first distance: ");
+scanf("%d%d", &d1.feet, &d1.inch);
+printf("Enter feet and inch of second distance: ");
+scanf("%d%d", &d2.feet, &d2.inch);
+sum.inch = d1.inch + d2.inch;
+sum.feet = d1.feet + d2.feet;
+if(sum.inch>=12)
+{
+sum.feet += sum.inch/12;
+sum.inch %= 12;
+}
+printf("Sum is %d' %d''", sum.feet, sum.inch);
+return 0;
+}
